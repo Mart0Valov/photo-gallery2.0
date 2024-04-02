f33482900e7a8a20b3644a5b5d6b1680d675c5c9
Martin Valov <maritn.valov98@gmail.com>
2024-04-02 23:49:26 +0300

    implementing getting all photos and their info data

--- photo-gallery-api/src/index.js
+++ photo-gallery-api/src/index.js
@@ -3,12 +3,22 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import multer from 'multer';
 import { PhotoInfo } from './dataBaseSchemas/photoInfoSchema.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = 3000;
 const app = express();
+// setting destination and what the file name for the photo must be
 const storage = multer.diskStorage({
     destination: 'photos/',
     filename: (request, file, cb) => {
+        // it takes the id of the photo info stored in the database
+        // and renames the file with it
         cb(null, request.params.id);
     }
 });
@@ -20,8 +30,10 @@ mongoose.connect('mongodb://localhost/photo-gallery')
     .then(() => console.log('Connected to database'))
     .catch((err) => console.error(err));
 
+// middleware
 app.use(express.json());
 app.use(cors());
+const photosDirectory = path.join(__dirname, '../photos');
 
 // upload image info
 app.post('/api/upload-image-info', async (request, response) => {
@@ -37,17 +49,26 @@ app.post('/api/upload-image-info', async (request, response) => {
 
 // upload photo endpoint
 app.post('/api/upload-image/:id', upload.single('file'), (request, response) => {
+    // sending response to the client that the file is saved successfully.
     return response.status(200).send({ msg: 'Photo info uploaded successfully' });
 });
 
 
-// get all photos endpoint
-app.get('/api/photos', (request, response) => {
-
+// get all photos info endpoint
+app.get('/api/photos-info', async (request, response) => {
+    const photosInfo = await PhotoInfo.find();
+    return response.status(200).send(photosInfo);
 });
 
-// get photo bt id endpoint
-app.get('/api/photo/:id', (request, response) => {
+// get photo by id endpoint
+app.get('/api/photo/:id', async (request, response) => {
+    const { params: { id } } = request;
+    const filePath = path.join(photosDirectory, id);
+    return response.sendFile(filePath);
+});
+
+// get photo info bt id endpoint
+app.get('/api/photo-info/:id', (request, response) => {
 
 });
 
